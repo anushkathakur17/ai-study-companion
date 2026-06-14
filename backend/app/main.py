3dbb3ae1f7689362d2b97f2aeb0c56726948ccc8
anushkathakur17 <anushka.thakur1701@gmail.com>
2026-06-14 19:09:41 +0530

    Setup authentication backend structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.routes.users import router as user_router
+
+from app.database import engine, Base
+from app.models import User
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 origins = [
     "http://localhost:5173"
@@ -15,6 +22,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(
+    user_router,
+    prefix="/users"
+)
+
 @app.get("/")
 def home():
-    return {"message": "AI Study Companion API Running"}
+    return {
+        "message":
+        "AI Study Companion API Running"
+    }
