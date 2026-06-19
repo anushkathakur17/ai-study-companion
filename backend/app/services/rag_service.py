7e1e9fdfc7b8e7effec2f8e2796552c7add29987
anushkathakur17 <anushka.thakur1701@gmail.com>
2026-06-19 11:46:36 +0530

    Reduced Render memory usage

--- backend/app/services/rag_service.py
+++ backend/app/services/rag_service.py
@@ -1,21 +1,45 @@
 from sentence_transformers import SentenceTransformer
+
 import chromadb
 
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+
+model = None
+
+
+def get_model():
+
+    global model
+
+    if model is None:
+
+        model = SentenceTransformer(
+
+            "all-MiniLM-L6-v2"
+
+        )
+
+    return model
+
 
 client = chromadb.PersistentClient(
+
     path="./chroma_db"
+
 )
 
 collection = client.get_or_create_collection(
+
     name="study_notes"
+
 )
 
+
 def chunk_text(
+
     text,
+
     chunk_size=700
+
 ):
 
     paragraphs = text.split("\n")
@@ -58,52 +82,80 @@ def chunk_text(
 
     return chunks
 
+
 def store_note_in_rag(
+
     note_id,
+
     text
+
 ):
 
+    model = get_model()
+
     chunks = chunk_text(
+
         text
+
     )
 
     embeddings = model.encode(
+
         chunks
+
     )
 
     for i in range(
+
         len(chunks)
+
     ):
 
         collection.add(
 
             ids=[
+
                 f"{note_id}_{i}"
+
             ],
 
             documents=[
+
                 chunks[i]
+
             ],
 
             embeddings=[
+
                 embeddings[i].tolist()
+
             ]
 
         )
 
+
 def retrieve_chunks(
+
     question,
+
     top_k=3
+
 ):
 
+    model = get_model()
+
     question_embedding = model.encode(
+
         question
+
     )
 
     results = collection.query(
 
         query_embeddings=[
+
             question_embedding.tolist()
+
         ],
 
         n_results=top_k
@@ -111,5 +163,7 @@ def retrieve_chunks(
     )
 
     return results[
+
         "documents"
+
     ][0]
